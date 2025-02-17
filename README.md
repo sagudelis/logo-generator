57a9f47017195c6ffe6c6e4b173c6502a46dd84c
saggydelis <60397021+sagudelis@users.noreply.github.com>
2025-02-17 15:33:53 -0500

    added standalone python file
    - renamed discord file to logomakerbot.py
    - created logomaker.py as standalone version
    - updated logomaker.py to work as standalone python file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # logo-generator
-Generates VT Hunt logos as .pngs, written to be used within a discord bot.
+Generates VT Hunt logos as .pngs for any given font.
+<br>----------------------------------------------------------------------------------<br>
+logomaker.py is a standalone file that can be run in python.<br>logomakerbot.py is written to be used within a discord bot.
--- logomakerbot.py
+++ logomakerbot.py
@@ -1,9 +1,12 @@
+# This program is intended to be used within a discord bot
 import io
 import os
 import requests
+import discord
 import warnings
 from zipfile import ZipFile
 from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageFilter
+from discord import app_commands
 
 path = './'
 # stop the deprecation warning from popping up
@@ -29,10 +32,9 @@ def text_to_image(text: str, font_filepath: str, font_size: int, color: tuple) -
 
     return img
 
-
 # function to merge two images
 def imageMerger(images, h_or_v, gap_os=0):
-    gap_os = round(gap_os / 2)
+    gap_os = round(gap_os/2)
 
     # get height and width of image canvas
     newImW = 0
@@ -51,14 +53,14 @@ def imageMerger(images, h_or_v, gap_os=0):
     for idx, image in enumerate(images):
         if 'h' in h_or_v:
             if image.size[1] < newImH:
-                hToAdd = round((newImH - image.size[1]) / 2)
+                hToAdd = round((newImH - image.size[1])/2)
                 new_h = image.size[1] + hToAdd + hToAdd
                 paddedIm = Image.new(image.mode, (image.size[0], new_h), (255, 255, 255, 0))
                 paddedIm.paste(image, (0, hToAdd))
                 images[idx] = paddedIm
         if 'v' in h_or_v:
             if image.size[0] < newImW:
-                wToAdd = round((newImW - image.size[0]) / 2)
+                wToAdd = round((newImW - image.size[0])/2)
                 new_w = image.size[0] + wToAdd + wToAdd
                 paddedIm = Image.new(image.mode, (new_w, image.size[1]), (255, 255, 255, 0))
                 paddedIm.paste(image, (wToAdd, 0))
@@ -81,28 +83,30 @@ def imageMerger(images, h_or_v, gap_os=0):
 
     return new_image
 
-
 # function to find the skinniest part of the top of the T
 def getTHeight(tImg):
+    # gets max height of left 1/4 of image
+    # t.crop(tuple((0, 0, round(t.size[0] / 4), t.size[1]))).getbbox()[3]
+
     # get the narrowest part of upper half of the image
-    uhFull = tImg.crop(tuple((0, 0, tImg.size[0], round(tImg.size[1] / 2))))  # get whole upper half image
+    uhFull = tImg.crop(tuple((0, 0, tImg.size[0], round(tImg.size[1]/2))))    # get whole upper half image
     uhFull = uhFull.crop(uhFull.getbbox())  # trim to bounding box
-    edgeSize = round(uhFull.size[0] / 10)  # get size of edges to trim off
-    upperHalf = uhFull.crop(tuple((edgeSize, 0, uhFull.size[0] - edgeSize, uhFull.size[1])))
+    edgeSize = round(uhFull.size[0]/10)     # get size of edges to trim off
+    upperHalf = uhFull.crop(tuple((edgeSize, 0, uhFull.size[0]-edgeSize, uhFull.size[1])))
 
     narrowest = tImg.size[1]
-    for vSliceInt in range(upperHalf.size[0] - 1):  # for each row of pixel
+    for vSliceInt in range(upperHalf.size[0]-1):      # for each row of pixel
         # make the single pixel vertical slice
-        vSlice = upperHalf.crop(tuple((vSliceInt, 0, vSliceInt + 1, upperHalf.size[1])))
+        vSlice = upperHalf.crop(tuple((vSliceInt, 0, vSliceInt+1, upperHalf.size[1])))
         # get the bounding box for the single pixel vertical slice
         bounds = vSlice.getbbox()
         if bounds is not None:
-            boxHeight = bounds[3] - bounds[1]  # the height of the bounding box
+            boxHeight = bounds[3] - bounds[1]    # the height of the bounding box
             if boxHeight < narrowest:
                 narrowest = boxHeight
 
+    # print(f'theight is {narrowest}')
     return narrowest
-
 
 # function to remove edges from image and optionally color
 def colorImage(image, color, trans):
@@ -122,7 +126,7 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
     path = './fonts'
     if not os.path.exists(path):
         os.makedirs(path)
-
+    
     try:
         R, G, B, A = map(int, RGBA.replace('(', '').replace(')', '').split(','))
         userColor = tuple((R, G, B))
@@ -132,7 +136,7 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
     userTrans = A
     allFonts = []
     fontNames = []
-
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.9',
@@ -140,7 +144,7 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Connection': 'keep-alive'
     }
-
+    
     if url:
         response = requests.get(url, headers=headers, allow_redirects=True)
         if response.status_code == 200:
@@ -168,7 +172,7 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
             print(f"Error downloading URL: {response.status_code}")
     elif file:
         file_like_object = file
-
+    
     if file:
         try:
             with ZipFile(file_like_object) as zf:
@@ -181,9 +185,8 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
                         fontNames.append(file_name.split('/')[-1].split('.')[0])
         except Exception as e:
             print(f"Error processing zip file: {e}")
-
+    
     allLogos = []
-    allFonts = set(allFonts)
     for idx, font in enumerate(allFonts):
         try:
             v = text_to_image(text='V', font_filepath=font, font_size=1000, color=tuple((255, 255, 255, 255)))
@@ -192,7 +195,7 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
                 print(f"Skipping font {fontNames[idx]} due to previous error.")
                 continue
             tHeight = getTHeight(tImg=t)
-            vcrop = v.crop(tuple((0, round(tHeight / 2), v.size[0], v.size[1])))
+            vcrop = v.crop(tuple((0, round(tHeight/2), v.size[0], v.size[1])))
             v = vcrop
 
             # Upper left
@@ -212,43 +215,45 @@ def makeTheLogo(url=None, file=None, RGBA="(255,255,255,255)", symmetry=True):
             t_bottom_right = ImageOps.mirror(t) if symmetry else t
 
             dbv = imageMerger([vm, v], 'vertical', gap_os=0)
-            dbt = imageMerger([tm, t], 'vertical', gap_os=round(tHeight * 2))
+            dbt = imageMerger([tm, t], 'vertical', gap_os=round(tHeight*2))
             dbv_right = imageMerger([v_upper_right, v_bottom_right], 'vertical', gap_os=0)
-            dbt_right = imageMerger([t_upper_right, t_bottom_right], 'vertical', gap_os=round(tHeight * 2))
+            dbt_right = imageMerger([t_upper_right, t_bottom_right], 'vertical', gap_os=round(tHeight*2))
 
             logo = imageMerger([dbv, dbt, dbt_right, dbv_right], 'horizontal', gap_os=tHeight)
             logo = colorImage(logo, userColor, userTrans)
-            logo.show()
-            # name = f'logo{idx}.png'       # to automatically save images as files
-            # logo.save(name)
-            # allLogos.append(name)
-
+            name = f'./logos/logo{idx}.png'
+            logo.save(name)
+            allLogos.append(name)
         except Exception as e:
             print(f'Error with {fontNames[idx]} font: {e}')
-
+    
     return allLogos
 
+def make_logo_command(tree: app_commands.CommandTree):
 
-def make_logo():
-    font = input('input the download URL or a zip file path for your desired font:\n\t')
-    rgba = input('input a color in RGBA format (optional, defaults to white):\n\t')
-    symmetry = input('should the VT be horizontally mirrored? (yes/no, optional, defaults to true):\n\t')
+    @tree.command(name="logoho", description="Create a logo from a URL or an uploaded font file")
+    @app_commands.describe(url="URL of the zip file containing fonts or a direct font file (optional)", file="Uploaded font file (optional)", rgba="RGBA color format", symmetry="Whether the V and T should be mirrored (optional, defaults to true)")
+    async def make_logo(interaction: discord.Interaction, rgba: str = "(255,255,255,255)", url: str = None, file: discord.Attachment = None, symmetry: bool = True):
+        if not url and not file:
+            await interaction.response.send_message("You must provide either a URL or upload a font file.", ephemeral=True)
+            return
 
-    # convert symmetry input to a boolean
-    if len(symmetry) > 0 and (symmetry[0].lower() == 'n' or symmetry[0].lower() == 'f'):
-        symmetry = False
-    else:
-        symmetry = True
+        if url and file:
+            await interaction.response.send_message("Please provide only one input, either a URL or a font file.", ephemeral=True)
+            return
 
-    try:
-        if '.zip' in font:
-            font = font.replace("'", "").replace('"','')
-            logos = makeTheLogo(file=font, RGBA=rgba, symmetry=symmetry)
-        else:
-            logos = makeTheLogo(url=font, RGBA=rgba, symmetry=symmetry)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        try:
+            if url:
+                logos = makeTheLogo(url=url, RGBA=rgba, symmetry=symmetry)
+            elif file:
+                font_bytes = await file.read()
+                file_like_object = io.BytesIO(font_bytes)
+                logos = makeTheLogo(file=file_like_object, RGBA=rgba, symmetry=symmetry)
 
-
-if __name__ == '__main__':
-    make_logo()
+            await interaction.response.send_message("Logos generated successfully. Sending files...", ephemeral=True)
+            
+            for logofile in logos:
+                with open(logofile, 'rb') as f:
+                    await interaction.followup.send(file=discord.File(f))
+        except Exception as e:
+            await interaction.followup.send(f"An error occurred: {e}", ephemeral=True)
